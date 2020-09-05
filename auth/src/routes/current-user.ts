3098b882d901a41e19f1394e63742f7a602921ea
malchimowicz <michal.alchimowicz@enovatis.pl>
2020-09-05 18:51:43 +0200

    signup settup

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-  res.send('HI Mike');
+  if (!req.session || !req.session.jwt) {
+    return res.send({ currentuser: null });
+  }
 });
 
 export { router as currentUserRouter };
