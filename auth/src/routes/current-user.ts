faeeeb6c88965003f457b98461e2cfd076675069
malchimowicz <michal.alchimowicz@enovatis.pl>
2020-09-06 12:08:58 +0200

    add curentUser

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -7,6 +7,13 @@ router.get('/api/users/currentuser', (req, res) => {
   if (!req.session || !req.session.jwt) {
     return res.send({ currentuser: null });
   }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({ currentuser: payload });
+  } catch (err) {
+    res.send({ currentuser: null });
+  }
 });
 
 export { router as currentUserRouter };
