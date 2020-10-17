a6d3b0518f221a12f935fd19c0b07bf7036f1b2c
malchimowicz <michal.alchimowicz@enovatis.pl>
2020-10-17 12:22:27 +0200

    testing

---
+++ tickets/src/routes/__test__/show.test.ts
@@ -0,0 +1,26 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
+
+it('return 404 if the ticket is not found', async () => {
+  await request(app).get('/api/tickets/lslslslflf').send().expect(404);
+});
+
+it('return the ticket if the ticket is found', async () => {
+  const title = 'concert';
+  const price = 20;
+
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({ title, price })
+    .expect(201);
+
+  const ticketResponse = await request(app)
+    .get(`/api/tickets/${response.body.id}`)
+    .send()
+    .expect(200);
+
+  expect(ticketResponse.body.title).toEqual(title);
+  expect(ticketResponse.body.price).toEqual(price);
+});
