a45edbeadf6a5d43cdd7af9e8f43d8dcc865f7c4
malchimowicz <michal.alchimowicz@enovatis.pl>
2020-10-26 08:53:24 +0100

    custom publisher

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,12 +1,15 @@
 import { Message } from 'node-nats-streaming';
 
 import { Listener } from './base-listener';
+import { TicketCreatedEvent } from './ticket-created-event';
+import { Subjects } from './subjects';
 
-export class TicketCreatedListener extends Listener {
-  subject = 'ticket:created';
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  //subject: Subjects.TicketCreated = Subjects.TicketCreated;
+  readonly subject = Subjects.TicketCreated;
   queueGroupName = 'payment-service';
 
-  onMessage(data: any, msg: Message) {
+  onMessage(data: TicketCreatedEvent['data'], msg: Message) {
     console.log('Event data', data);
     msg.ack();
   }
