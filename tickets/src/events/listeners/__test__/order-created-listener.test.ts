51adbec09f5f3d2cd4d8c5c4143c5db6e0d42955
malchimowicz <michal.alchimowicz@enovatis.pl>
2020-11-28 11:28:15 +0100

    update event, mock functions, order cancelled listener, tests

--- tickets/src/events/listeners/__test__/order-created-listener.test.ts
+++ tickets/src/events/listeners/__test__/order-created-listener.test.ts
@@ -52,3 +52,17 @@ it('acks the message', async () => {
   await lisener.onMessage(data, msg);
   expect(msg.ack).toHaveBeenCalled();
 });
+
+it('publishes a ticket updated event', async () => {
+  const { lisener, ticket, data, msg } = await setup();
+
+  await lisener.onMessage(data, msg);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+
+  const ticketUpdatedData = JSON.parse(
+    (natsWrapper.client.publish as jest.Mock).mock.calls[0][1]
+  );
+
+  expect(data.id).toEqual(ticketUpdatedData.orderId);
+});
